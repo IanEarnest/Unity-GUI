cb39867477533da1c4b44290760e8267d84d45d1
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-09-09 00:33:54 +0100

    Text 3D cleaned up
    Still a bug left with clear text

--- Assets/Text 3D/Text3DChange.cs
+++ Assets/Text 3D/Text3DChange.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class Text3DChange : MonoBehaviour {
 
-
+	
+	public TextMesh change1Btn;
+	public TextMesh change2Btn;
 	Text3DFind ScriptFindText = new Text3DFind();
 	TextMesh textToChange1;
 	TextMesh textToChange2;
-	
-	Text3DClear ScriptClearText = new Text3DClear();
-	public TextMesh textToClear;
+	bool changeText1Active;
+	bool changeText2Active;
 
+	public TextMesh clearBtn;
+	Text3DClear ScriptClearText = new Text3DClear();
+	public TextMesh textToClear1;
+	public TextMesh textToClear2;
+	bool clearTextActive;
+
+	Ray ray;
+	RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
@@ -21,9 +30,43 @@ public class Text3DChange : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		textToChange1.text = "Changed 1";
-		textToChange2.text = "Changed 2";
+		change1Btn.fontStyle = FontStyle.Normal;
+		change2Btn.fontStyle = FontStyle.Normal;
+		clearBtn.fontStyle = FontStyle.Normal;
 
-		ScriptClearText.clearText (textToClear);
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit)){
+			if(hit.transform.name == change1Btn.name){
+				change1Btn.fontStyle = FontStyle.Bold;
+				if(Input.GetMouseButtonDown(0)){
+					changeText1Active = true;
+				}
+			}
+			if(hit.transform.name == change2Btn.name){
+				change2Btn.fontStyle = FontStyle.Bold;
+				if(Input.GetMouseButtonDown(0)){
+					changeText2Active = true;
+				}
+			}
+			if(hit.transform.name == clearBtn.name){
+				clearBtn.fontStyle = FontStyle.Bold;
+				if(Input.GetMouseButtonDown(0)){
+					clearTextActive = true;
+				}
+			}
+		}
+
+
+		if(changeText1Active == true){
+			textToChange1.text = "Changed 1";
+		}
+		if(changeText2Active == true){
+			textToChange2.text = "Changed 2";
+		}
+		if(clearTextActive == true){
+			ScriptClearText.clearText (textToClear1);
+		}
+
+		ScriptClearText.clearText (textToClear2);
 	}
 }
