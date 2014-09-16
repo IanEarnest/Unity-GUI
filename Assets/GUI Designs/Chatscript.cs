62bf1fb90ce23e822ff5ae63ed115f547e328d68
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-09-16 23:57:12 +0100

    Update GUI Designs

---
+++ Assets/GUI Designs/Chatscript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chatscript : MonoBehaviour {
+
+	Rect window1Rect = new Rect(Screen.width/2-75, Screen.height-200, 150, 50);
+
+	string chat = "hi";
+	string message = "";
+
+	void OnGUI(){
+		window1Rect.y = Screen.height - window1Rect.height;
+		if (GUIDesigns.guiSelect == 2) {
+			window1Rect = GUILayout.Window(1, window1Rect, window1Function, "Chat");
+		}
+	}
+
+	
+	// Window 1
+	void window1Function(int id){
+		GUILayout.Label (chat);
+		GUILayout.Space (90);
+
+		message = GUILayout.TextArea(message);
+		if(GUILayout.Button("Send")){
+			chat += "\n"+message;
+			message = "";
+		}
+	}
+}
--- Assets/GUI Designs/Interface.cs
+++ Assets/GUI Designs/Interface.cs
@@ -1,35 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GUIDesigns : MonoBehaviour {
+public class Interface : MonoBehaviour {
 
-	public static int guiSelect = 0;
-	Rect backRect = new Rect(Screen.width-100, Screen.height/2, 100, 50);
-
-	//Rect window1Rect = new Rect(Screen.width/2-75, Screen.height/2-100, 150, 50);
+	Rect window1Rect = new Rect(Screen.width/2-75, Screen.height/2-100, 150, 50);
 
 	void OnGUI(){
-		if(guiSelect != 0){
-			if(GUI.Button(backRect, "GUI Select")){
-				guiSelect = 0;
-			}
-		}
-
-		if(guiSelect == 0){
-			GUILayout.Label ("GUI Select");
-			if(GUILayout.Button("GUI 1")){
-				guiSelect = 1;
-			}
-			if(GUILayout.Button("GUI 2")){
-				guiSelect = 2;
-			}
-			if(GUILayout.Button("GUI 3")){
-				guiSelect = 3;
-			}
-		}
-
-		/*
-		if (guiSelect == 1) {
+		if (GUIDesigns.guiSelect == 1) {
 			window1Rect = GUILayout.Window(1, window1Rect, window1Function, "Main Menu");
 
 			GUILayout.BeginHorizontal();
@@ -39,11 +16,11 @@ public class GUIDesigns : MonoBehaviour {
 				GUILayout.Space (50);
 				GUILayout.Label("Normal");
 			GUILayout.EndHorizontal();
-		}*/
+		}
 
 	}
 
-	/*
+	
 	// Window 1
 	void window1Function(int id){
 		if(GUILayout.Button("Continue")){
@@ -69,5 +46,5 @@ public class GUIDesigns : MonoBehaviour {
 		if(GUILayout.Button("Logout")){
 			
 		}
-	}*/
+	}
 }
