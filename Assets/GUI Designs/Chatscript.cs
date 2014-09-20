9f055e5952979525b4d85003ff3d659c5346b9a8
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-09-20 10:49:28 +0100

    Finish GUI Designs chatscript

--- Assets/GUI Designs/Chatscript.cs
+++ Assets/GUI Designs/Chatscript.cs
@@ -3,28 +3,53 @@ using System.Collections;
 
 public class Chatscript : MonoBehaviour {
 
-	Rect window1Rect = new Rect(Screen.width/2-75, Screen.height-200, 150, 50);
+	Rect chatWindow = new Rect(Screen.width/2-400, Screen.height-200, 800, 50);
 
-	string chat = "hi";
+	string chat = "";
 	string message = "";
 
 	void OnGUI(){
-		window1Rect.y = Screen.height - window1Rect.height;
+
+
+		//window1Rect.y = Screen.height - window1Rect.height;
 		if (GUIDesigns.guiSelect == 2) {
-			window1Rect = GUILayout.Window(1, window1Rect, window1Function, "Chat");
+			chatWindow = GUILayout.Window(1, chatWindow, chatFunction, "");
 		}
 	}
 
-	
-	// Window 1
-	void window1Function(int id){
-		GUILayout.Label (chat);
-		GUILayout.Space (90);
+	Vector2 scrollPos;
 
-		message = GUILayout.TextArea(message);
-		if(GUILayout.Button("Send")){
-			chat += "\n"+message;
+	// Window 1
+	void chatFunction(int id){
+		scrollPos = GUILayout.BeginScrollView (scrollPos, false, true, GUILayout.Width(780), GUILayout.Height(135));
+			GUILayout.Box (chat);
+		GUILayout.EndScrollView ();
+		GUILayout.Space (10);
+
+		GUILayout.BeginHorizontal ();
+			message = GUILayout.TextField(message);
+			
+		/*
+			if(GUILayout.Button("Send", GUILayout.Width(100))){
+				sendMessageToChat();
+			}
+			if (Input.GetKeyDown(KeyCode.Return)) {
+				sendMessageToChat();
+			}
+		*/
+			if (GUILayout.Button ("Send", GUILayout.Width (100)) || Event.current.type == EventType.KeyDown) {
+				sendMessageToChat();
+			}
+
+		GUILayout.EndHorizontal ();
+	}
+
+	void sendMessageToChat(){
+		// add new line to each message
+		if(message != ""){
+			chat += "\nYou said: "+message;
 			message = "";
+			scrollPos.y = 10000;
 		}
 	}
 }
