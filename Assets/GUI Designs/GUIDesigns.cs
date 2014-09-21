b38ef833454e15cbf76f3293626931925ce9c115
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-09-21 11:22:13 +0100

    Update GUI Designs
    Changed menu

--- Assets/GUI Designs/GUIDesigns.cs
+++ Assets/GUI Designs/GUIDesigns.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class GUIDesigns : MonoBehaviour {
 
 	public static int guiSelect = 0;
-	Rect backRect = new Rect(Screen.width-80, Screen.height-100, 80, 30);
+	Rect guiRect = new Rect(Screen.width-80, Screen.height/2-75, 80, 150);
+	//backRect = new Rect(Screen.width-80, Screen.height-100, 80, 30);
 
 	void OnGUI(){
+
+
+
 		// GUI selection
-		if(guiSelect == 0){
+		//if(guiSelect == 0){
+		GUILayout.BeginArea (guiRect);
 			GUILayout.Label ("GUI Select");
+			if(GUILayout.Button("None")){
+				guiSelect = 0;
+			}
 			if(GUILayout.Button("Interface")){
 				guiSelect = 1;
 			}
@@ -22,13 +30,16 @@ public class GUIDesigns : MonoBehaviour {
 			if(GUILayout.Button("Lobby 2")){
 				guiSelect = 4;
 			}
-		}
+		GUILayout.EndArea ();
+		//}
 
 		// Back button
+		/*
 		if(guiSelect != 0){
 			if(GUI.Button(backRect, "GUI Select")){
 				guiSelect = 0;
 			}
 		}
+		 */
 	}
 }
