b71f0c00102e0e8cb4dfe544c4b843884d0c3b15
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-09-21 12:29:31 +0100

    Create menu images

---
+++ Assets/Images.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Images : MonoBehaviour {
+
+	public Texture[] images = new Texture[9];
+
+	// Use this for initialization
+	void Start () {
+		for (int a = 0; a < images.Length; a++) {
+			int b = a+1;
+			images[a] = Resources.Load(b.ToString()) as Texture;
+		}
+	}
+}
